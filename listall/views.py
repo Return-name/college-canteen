8a685a8fc96f61f4703c885b09a6616303c1d9a7
Return-name <rishabha649@gmail.com>
2021-03-27 00:24:21 +0530

    Updated /listall index to show all Items

--- listall/views.py
+++ listall/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Item
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello. Here is the list of all items in the canteen")
+    output = "<html><center>Hello. Here is the list of all items in the canteen<br>"
+    items = Item.objects.all()
+    output += "<table><th>Name</th><th>Price</th><th>Quantity</th>"
+    for i in items:
+        output += "<tr><td>"+i.name + "</td><td>" + str(i.price) + "</td><td>" + str(i.quantity) + "</td></tr>"
+    output += "</table></center><html>";
+    return HttpResponse(output)
 
