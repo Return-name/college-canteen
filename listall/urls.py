e9dcd5883c9dda686591da60bebc12b23e5dc35f
Return-name <rishabha649@gmail.com>
2021-03-28 03:08:44 +0530

    Added Category Wise Item Display and buttons for nav

--- listall/urls.py
+++ listall/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('category/', views.category_index, name='category_index'),
         ]
--- listall/views.py
+++ listall/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .models import Item
+from .models import Item, Category
 # Create your views here.
 
 def index(request):
-    output = "<html><center>Hello. Here is the list of all items in the canteen<br>"
-    items = Item.objects.all()
-    output += "<table border=2><th>Image</th><th>Name</th><th>Price</th><th>Quantity</th>"
-    for i in items:
-        output += "<tr>"
-        if i.image:
-            output += "<td>"+i.image_tag()+"</td>"
-        else:
-            output += "<td>[Img Not Avl]</td>"
-        output += "<td>"+i.name + "</td><td>" + str(i.price) + "</td><td>" + str(i.quantity) + "</td></tr>"
-    output += "</table></center><html>";
-    return HttpResponse(output)
+
+    context = {'items': Item.objects.all() }
+    return render(request, 'listall/index.html', context)
+    
+def category_index(request):
+    context = {
+            'Categories': Category.objects.all(),
+            'CategoryItems': [cat.item_set.all() for cat in Category.objects.all()],
+            }
+
+    return render(request, 'listall/category_index.html', context)
