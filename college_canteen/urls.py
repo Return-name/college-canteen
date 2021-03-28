626f673d7f6aec673d052be0d9df9a7ab20edfb3
Return-name <rishabha649@gmail.com>
2021-03-28 18:17:12 +0530

    Added HomePage

--- college_canteen/urls.py
+++ college_canteen/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('foodfun.urls')),
     path('listall/', include('listall.urls')),
     path('admin/', admin.site.urls),
 ]
